4c65f221216415c9b17c288d0ef0f694bc225f9e
James Maynard <jameskirkmaynard@gmail.com>
2025-06-12 13:13:49 -0700

    handle redirects to https

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http'
+          }
+        ],
+        destination: 'https://jamesmaynardpoetry.com/:path',
+        permanent: true
+      }
+    ]
+  }
 };
 
 export default nextConfig;
